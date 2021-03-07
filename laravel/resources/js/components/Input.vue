a2f61cef93889d3c903039e8afe6bc9fce54ed76
k-anagosan <k.anagosan1027@gmail.com>
2021-03-08 00:37:48 +0900

    [add]#11 Create a Register Form and a Login Form
    Loginコンポーネント内にRegisterフォームとLoginフォームを作成しました。
    （タブで表示を切り替えることにしたため）

---
+++ laravel/resources/js/components/Input.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="mb-4">
+    <label :for="id" class="inline-block mb-1">{{ label }}</label>
+    <input
+      :id="id"
+      :type="type"
+      :value="val"
+      class="block w-full relative border-0 border-b-2 border-gray-300 text-lg focus:outline-none focus:border-gray-500 transition"
+      @input="onInput"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: "text",
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      val: this.value,
+    };
+  },
+  methods: {
+    onInput($e) {
+      this.val = $e.target.value;
+      this.$emit("input", this.val);
+    },
+  },
+};
+</script>
+
+<style></style>
--- laravel/resources/js/pages/Login.vue
+++ laravel/resources/js/pages/Login.vue
@@ -1,3 +1,118 @@
 <template>
-  <h1>Login</h1>
+  <div class="max-w-screen-sm mx-auto">
+    <ul class="flex justify-start items-center h-12">
+      <li class="h-full mr-2 relative">
+        <button
+          id="login-tab"
+          :class="{ 'text-gray-900 font-bold': tab === 1 }"
+          class="flex justify-center items-center h-full px-4 text-gray-500 outline-none focus:outline-none"
+          @click="tab = 1"
+        >
+          Login
+        </button>
+        <span
+          class="absolute bottom-0 h-0.5 w-full bg-gray-300"
+          :class="{ 'bg-gray-900': tab === 1 }"
+        ></span>
+      </li>
+      <li class="h-full mr-2 relative">
+        <button
+          id="register-tab"
+          :class="{ 'text-gray-900 font-bold': tab === 2 }"
+          class="flex justify-center items-center h-full px-4 text-gray-500 outline-none focus:outline-none"
+          @click="tab = 2"
+        >
+          Register
+        </button>
+        <span
+          class="absolute bottom-0 h-0.5 w-full bg-gray-300"
+          :class="{ 'bg-gray-900': tab === 2 }"
+        ></span>
+      </li>
+    </ul>
+    <div v-show="tab === 1" class="pt-4">
+      <form id="login-form" @submit.prevent="login">
+        <Input
+          id="login-email"
+          v-model="loginForm.email"
+          type="text"
+          label="Email"
+        />
+        <Input
+          id="login-password"
+          v-model="loginForm.password"
+          type="password"
+          label="Password"
+        />
+        <div class="flex justify-end">
+          <Button id="login-btn" type="submit">login</Button>
+        </div>
+      </form>
+    </div>
+    <div v-show="tab === 2" class="pt-4">
+      <form id="register-form" @submit.prevent="register">
+        <Input
+          id="username"
+          v-model="registerForm.username"
+          type="text"
+          label="Username"
+        />
+        <Input
+          id="email"
+          v-model="registerForm.email"
+          type="text"
+          label="Email"
+        />
+        <Input
+          id="password"
+          v-model="registerForm.password"
+          type="password"
+          label="Password"
+        />
+        <Input
+          id="password-confirmation"
+          v-model="registerForm.passwordConfirmation"
+          type="password"
+          label="Password (Confirm)"
+        />
+        <div class="flex justify-end">
+          <Button id="register-btn" type="submit">register</Button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+<script>
+import Input from "../components/Input.vue";
+import Button from "../components/Button.vue";
+
+export default {
+  components: {
+    Input,
+    Button,
+  },
+  data() {
+    return {
+      tab: 1,
+      loginForm: {
+        email: "",
+        password: "",
+      },
+      registerForm: {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.loginForm);
+    },
+    register() {
+      console.log(this.registerForm);
+    },
+  },
+};
+</script>
