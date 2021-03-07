b964f42093978839823e73d11cec70806f965e5a
k-anagosan <k.anagosan1027@gmail.com>
2021-03-08 00:35:06 +0900

    [update]#11 Fix layout
    ・Headerコンポーネントのnavのpadding-xを修正
    ・focus:outline-noneを一部削除
    ・Appコンポーネントのmainのpadding-xtを修正

--- laravel/resources/js/components/Footer.vue
+++ laravel/resources/js/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer
     class="flex absolute bottom-0 items-center justify-center h-20 sm:h-24 w-full bg-blue-400"
   >
-    <button class="px-4 focus:outline-none">Logout</button>
+    <button class="px-4">Logout</button>
     <RouterLink to="/login" class="px-4"> Login / Register </RouterLink>
   </footer>
 </template>
