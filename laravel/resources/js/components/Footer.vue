6ce8cf4c70a5491d3956adb1f2ceb16343279126
k-anagosan <k.anagosan1027@gmail.com>
2021-03-07 18:40:46 +0900

    [add]#36 Create a Header component and a Footer component
    Header.vueとFooter.vueを作成しApp.vueに適用しました。

---
+++ laravel/resources/js/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer
+    class="flex absolute bottom-0 items-center justify-center h-20 sm:h-24 w-full bg-blue-400"
+  >
+    <button class="px-4 focus:outline-none">Logout</button>
+    <RouterLink to="/login" class="px-4"> Login / Register </RouterLink>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
