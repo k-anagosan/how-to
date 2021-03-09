db82f87e0fae68a761e5a899833a13440df36a44
k-anagosan <k.anagosan1027@gmail.com>
2021-03-10 01:01:58 +0900

    [add]#44 Created a logout() method
    * ログアウトボタンが押されたときに、auth/logoutアクションを実行するlogout()メソッドを作成しました。

--- laravel/resources/js/components/Footer.vue
+++ laravel/resources/js/components/Footer.vue
@@ -2,13 +2,21 @@
   <footer
     class="flex absolute bottom-0 items-center justify-center h-20 sm:h-24 w-full bg-blue-400"
   >
-    <button class="px-4">Logout</button>
+    <button class="px-4" @click="logout">Logout</button>
     <RouterLink to="/login" class="px-4"> Login / Register </RouterLink>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    async logout() {
+      await this.$store.dispatch("auth/logout");
+
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style></style>
