9271c7166f794a2b4ae45da59495d8d18caa9a64
k-anagosan <k.anagosan1027@gmail.com>
2021-03-09 22:29:22 +0900

    [add]#9 Created a test code to validate the register action in the auth store
    ・auth.jsのloginアクションを実行したら、データがstate.userに正しく保存されるか検証するテストを作成しました。

--- laravel/resources/js/store/auth.test.js
+++ laravel/resources/js/store/auth.test.js
@@ -1,6 +1,7 @@
 import Vuex from "vuex";
 import { createLocalVue } from "@vue/test-utils";
 import "@/bootstrap";
+import { randomStr } from "@/utils";
 
 import auth from "@/store/auth";
 
@@ -22,13 +23,16 @@ describe("store/auth.js", () => {
         windowSpy.mockImplementation(() => ({
             ...originalWindow,
             axios: {
-                post: (url, data) => ({
-                    data: {
-                        name: data.name,
-                        email: data.email,
-                        id: 1,
-                    },
-                }),
+                post: (url, data) => {
+                    const name = data.name ?? "testuser";
+                    return {
+                        data: {
+                            name,
+                            email: data.email,
+                            id: 1,
+                        },
+                    };
+                },
             },
         }));
     });
@@ -47,19 +51,35 @@ describe("store/auth.js", () => {
 
         it("registerアクションによりstate.userに正しく値が保存されるか", async done => {
             action = "register";
-            await testedAction(
-                { commit, state },
-                {
-                    name: "test",
-                    email: "test@email.com",
-                    password: "password",
-                    password_confirmation: "password",
-                }
-            );
+            const data = {
+                name: randomStr(),
+                email: `${randomStr()}@${randomStr()}.com`,
+                password: "password",
+                password_confirmation: "password",
+            };
+
+            await testedAction({ commit, state }, data);
             expect(store.state.user).toEqual({
-                email: "test@email.com",
+                email: data.email,
                 id: expect.anything(),
-                name: "test",
+                name: data.name,
+            });
+            done();
+        });
+
+        it("loginアクションによりstate.userに正しく値が保存されるか", async done => {
+            action = "login";
+            const data = {
+                email: `${randomStr()}@${randomStr()}.com`,
+                password: "password",
+            };
+
+            await testedAction({ commit, state }, data);
+
+            expect(store.state.user).toEqual({
+                email: data.email,
+                id: expect.anything(),
+                name: "testuser",
             });
             done();
         });
