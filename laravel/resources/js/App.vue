ddb6f2c42cbadcf80fbebedd2944a633f1e405bb
k-anagosan <k.anagosan1027@gmail.com>
2021-03-15 09:22:36 +0900

    [update]#63 Add a process to redirect to "/500" if the status code is "500"
    * ステータスコード500をerrorストアのerorrCodeが受け取ったら、"/500"にリダイレクトする処理をApp.vueに追加しました。

--- laravel/resources/js/App.vue
+++ laravel/resources/js/App.vue
@@ -11,11 +11,30 @@
 <script>
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import { INTERNAL_SERVER_ERROR } from "./utils";
 
 export default {
   components: {
     Header,
     Footer,
+  },
+  computed: {
+    errorCode() {
+      return this.$store.state.error.errorCode;
+    },
+  },
+  watch: {
+    errorCode: {
+      handler(errorCode) {
+        if (errorCode === INTERNAL_SERVER_ERROR) {
+          this.$router.push("/500");
+        }
+      },
+      immediate: true,
+    },
+    $route() {
+      this.$store.commit("error/setErrorCode", null);
+    },
   },
 };
 </script>
