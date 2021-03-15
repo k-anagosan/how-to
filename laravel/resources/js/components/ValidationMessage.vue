475d3c3bb4053ff6dd273e3df9f810923dd95767
k-anagosan <k.anagosan1027@gmail.com>
2021-03-16 05:16:48 +0900

    [add]#67 Created a component for displaying validation errors
    * バリデーションエラーをpropsとして受け取り内容を表示するコンポーネントValidationMessage.vueを作成しました。

---
+++ laravel/resources/js/components/ValidationMessage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="errors">
+    <ul v-if="errors[element]">
+      <li v-for="msg in errors[element]" :key="msg">{{ msg }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    errors: {
+      type: Object,
+      required: false,
+      default() {
+        return null;
+      },
+    },
+    element: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
