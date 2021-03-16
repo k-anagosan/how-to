0618ea168022359545ecf719bcef6a2f3846f8fc
k-anagosan <k.anagosan1027@gmail.com>
2021-03-16 15:11:38 +0900

    [update]#67 Created a test code to verify that Login.vue correctly receives the validation error
    * Login.vueがauthモジュールのバリデーションエラーを正しく受け取っていることを検証しました。
    * ページアクセス時や、タブ切り替えによりauthモジュールのバリデーションエラーをクリアするclearMessage()が実行されるかを検証しました。

--- laravel/resources/js/pages/Login.test.js
+++ laravel/resources/js/pages/Login.test.js
@@ -24,8 +24,12 @@ const authStoreMock = {
         login: jest.fn(),
     },
     mutations: {
-        setLoginValidationMessage: jest.fn(),
-        setRegisterValidationMessage: jest.fn(),
+        setLoginValidationMessage: jest.fn().mockImplementation(state => {
+            state.loginValidationMessage = null;
+        }),
+        setRegisterValidationMessage: jest.fn().mockImplementation(state => {
+            state.registerValidationMessage = null;
+        }),
     },
 };
 
@@ -130,17 +134,43 @@ describe("Vuex", () => {
     });
 
     describe("異常終了", () => {
+        let store = null;
+        const computedValue = target =>
+            Login.computed[target].call({ $store: store });
+        const spyClearMessage = jest.spyOn(Login.methods, "clearMessage");
+
         beforeEach(async () => {
             authStoreMock.state.apiIsSuccess = false;
-            const store = new Vuex.Store({
+            authStoreMock.state.loginValidationMessage = {
+                email: [randomStr()],
+                password: [randomStr()],
+            };
+            authStoreMock.state.registerValidationMessage = {
+                name: [randomStr()],
+                email: [randomStr()],
+                password: [randomStr(), randomStr()],
+            };
+
+            store = new Vuex.Store({
                 modules: {
                     auth: authStoreMock,
                 },
             });
 
-            wrapper = shallowMount(Login, { store, router, localVue });
+            wrapper = shallowMount(Login, {
+                store,
+                router,
+                localVue,
+            });
             await wrapper.vm.$router.push("/login").catch(() => {});
         });
+
+        afterEach(() => {
+            authStoreMock.mutations.setLoginValidationMessage.mock.calls = [];
+            authStoreMock.mutations.setRegisterValidationMessage.mock.calls = [];
+            spyClearMessage.mock.calls = [];
+        });
+
         it("registerの結果422エラーの時はリダイレクトしない", async () => {
             expect(wrapper.vm.$route.path).toBe("/login");
 
@@ -156,6 +186,65 @@ describe("Vuex", () => {
 
             expect(authStoreMock.actions.login).toHaveBeenCalled();
             expect(wrapper.vm.$route.path).toBe("/login");
+        });
+
+        it("#login-formのバリデーションエラーを正しく算出している", () => {
+            const errors = computedValue("loginErrors");
+            expect(errors).toEqual(authStoreMock.state.loginValidationMessage);
+        });
+
+        it("#register-formのバリデーションエラーを正しく算出している", () => {
+            const errors = computedValue("registerErrors");
+
+            expect(errors).toEqual(
+                authStoreMock.state.registerValidationMessage
+            );
+
+            // registerがstoreからloginValidationMessageを受け取っていることを確認できればいい
+        });
+        it("clearMessage()により、authのloginValidationMessageとregisterValidationMessageがクリアされる", () => {
+            // computedの各値を取得
+            let loginErrors = computedValue("loginErrors");
+            let registerErrors = computedValue("registerErrors");
+
+            // computedの各値がauth.stateの対応するものと等しい
+            expect(loginErrors).toEqual(
+                authStoreMock.state.loginValidationMessage
+            );
+            expect(registerErrors).toEqual(
+                authStoreMock.state.registerValidationMessage
+            );
+
+            authStoreMock.mutations.setLoginValidationMessage.mock.calls = [];
+            authStoreMock.mutations.setRegisterValidationMessage.mock.calls = [];
+
+            // mutationを実行
+            wrapper.vm.clearMessage();
+
+            // 各mutationが実行されたかチェック
+            expect(
+                authStoreMock.mutations.setLoginValidationMessage
+            ).toHaveBeenCalledTimes(1);
+            expect(
+                authStoreMock.mutations.setRegisterValidationMessage
+            ).toHaveBeenCalledTimes(1);
+
+            // 更新後のcomputedの値を取得
+            loginErrors = computedValue("loginErrors");
+            registerErrors = computedValue("registerErrors");
+
+            // clearMessage()によってcomputedの値がクリアされたことをチェック
+            expect(loginErrors).toBe(null);
+            expect(registerErrors).toBe(null);
+        });
+        it("ページアクセス時にclearMessage()が呼び出されている", () => {
+            expect(spyClearMessage).toHaveBeenCalledTimes(1);
+        });
+        it("タブ切り替えによりclearMessage()が呼び出されている", async () => {
+            spyClearMessage.mock.calls = [];
+            expect(spyClearMessage).not.toHaveBeenCalled();
+            await wrapper.setData({ tab: 2 });
+            expect(spyClearMessage).toHaveBeenCalled();
         });
     });
 });
