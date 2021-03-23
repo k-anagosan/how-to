8a0e33e2b4c51dd1df3de14721433ffbbb4db0f4
k-anagosan <k.anagosan1027@gmail.com>
2021-03-23 22:11:32 +0900

    [update]#81 Updated PostSubmitApiTest
    * 画像付きの記事を投稿した時の処理を検証するテストコードを追加しました。

--- laravel/tests/Feature/PostSubmitApiTest.php
+++ laravel/tests/Feature/PostSubmitApiTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -56,7 +55,7 @@ class PostSubmitApiTest extends TestCase
         $this->assertMatchesRegularExpression('/^[0-9a-zA-Z]{20}$/', $response->json('post_id'));
 
         // アップロードによりPostsテーブルにレコードが保存されたか
-        $post = Post::first();
+        $post = $this->user->posts->first();
         $this->assertMatchesRegularExpression('/^[0-9a-zA-Z]{20}$/', $post->id);
         $this->assertDatabaseHas($post->getTable(), [
             'title' => $this->data['title'],
@@ -67,5 +66,48 @@ class PostSubmitApiTest extends TestCase
         // S3に本文がファイルとして保存されているか
         Storage::cloud()->assertExists($post->content);
         $this->assertEquals(Storage::cloud()->get($post->content), $this->data['content']);
+    }
+
+    /**
+     * @test
+     */
+    public function should_画像付きの記事をアップロードできる(): void
+    {
+        $this->data['tags'] = null;
+
+        // アップロードを行う
+        $response = $this->actingAs($this->user)->postJson(route('post.create'), $this->data);
+
+        $response->assertStatus(201);
+
+        // レスポンスJSONが期待した構造であるか
+        $this->assertMatchesRegularExpression('/^[0-9a-zA-Z]{20}$/', $response->json('post_id'));
+
+        // アップロードによりPostsテーブルにレコードが保存されたか
+        $post = $this->user->posts->first();
+        $this->assertMatchesRegularExpression('/^[0-9a-zA-Z]{20}$/', $post->id);
+        $this->assertDatabaseHas($post->getTable(), [
+            'title' => $this->data['title'],
+            'content' => $post->content,
+            'user_id' => $this->user->id,
+        ]);
+
+        // S3に本文がファイルとして保存されているか
+        Storage::cloud()->assertExists($post->content);
+        $this->assertEquals(Storage::cloud()->get($post->content), $this->data['content']);
+
+        // Photoモデルの配列（？）を取得
+        $photos = $post->photos;
+
+        foreach ($photos->all() as $photo) {
+            // 画像ファイル名がDBに保存されたか
+            $this->assertDatabaseHas($photo->getTable(), [
+                'post_id' => $photo->post_id,
+                'filename' => $photo->filename,
+            ]);
+
+            // 画像ファイルがS3に保存されたか
+            Storage::cloud()->assertExists($post->filename);
+        }
     }
 }
