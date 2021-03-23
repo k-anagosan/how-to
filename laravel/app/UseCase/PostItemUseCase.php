9f5a8dd5b78b55b75d6e55f135228a29b97a098b
k-anagosan <k.anagosan1027@gmail.com>
2021-03-23 23:04:39 +0900

    [update]#81 Updated PostItemUseCase
    * PhotoServiceをコンストラクタインジェクションしました。
    * 画像ファイルを永続化するワークフローを記述しました。

--- laravel/app/UseCase/PostItemUseCase.php
+++ laravel/app/UseCase/PostItemUseCase.php
@@ -2,10 +2,12 @@
 
 namespace App\UseCase;
 
+use App\Domain\Photo\Service\PhotoService;
 use App\Domain\Post\Service\PostItemService;
 use App\Domain\User\Service\AuthorService;
 use App\Domain\ValueObject\PostContent;
 use App\Domain\ValueObject\PostId;
+use App\Domain\ValueObject\PostPhotos;
 use App\Domain\ValueObject\PostTitle;
 
 final class PostItemUseCase
@@ -14,26 +16,36 @@ final class PostItemUseCase
 
     private $postItemService;
 
+    private $photoService;
+
     public function __construct(
         AuthorService $authorService,
-        PostItemService $postItemService
+        PostItemService $postItemService,
+        PhotoService $photoService
     ) {
         $this->authorService = $authorService;
         $this->postItemService = $postItemService;
+        $this->photoService = $photoService;
     }
 
     /**
      * @param PostTitle   $title
      * @param PostContent $content
+     * @param PostPhotos  $photos
      *
      * @return PostId
      */
     public function execute(
         PostTitle $title,
-        PostContent $content
-    ): PostId {
+        PostContent $content,
+        PostPhotos $photos
+    ) {
         $author = $this->authorService->getAuthor();
+
         $postedItem = $author->postItem($title, $content);
+        $postedPhotos = $postedItem->postPhotos($photos);
+
+        $this->photoService->savePhotos($postedPhotos);
         return $this->postItemService->saveItem($postedItem);
     }
 }
