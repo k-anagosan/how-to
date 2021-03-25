82c310671f7ecf95e0e2d12fd14eb37e85d03191
k-anagosan <k.anagosan1027@gmail.com>
2021-03-25 23:14:20 +0900

    [update]#82 Updated the fillable property of the TagName model
    * TagNameモデルのfillableプロパティにnameカラムを追加しました。

--- laravel/app/Models/TagName.php
+++ laravel/app/Models/TagName.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TagName extends Model
 {
+    protected $fillable = ['name'];
+
     public function tags()
     {
         return $this->hasMany(Tag::class);
