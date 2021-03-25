15cf7adbf88f6740569800fb0659132bed3fc2f8
k-anagosan <k.anagosan1027@gmail.com>
2021-03-25 18:51:59 +0900

    [add]#82 Created a ValueObject that can hold multiple PostTag
    * 複数のPostTagオブジェクトを配列のプロパティとして保存できる、PostTagsクラスを作成しました。

---
+++ laravel/app/Domain/ValueObject/PostTags.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Domain\ValueObject;
+
+class PostTags
+{
+    private $tags;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * PostTagsの配列もしくはnullをプロパティとして保持する自インスタンスを作成.
+     *
+     * @param mixed $primitiveTags
+     */
+    public static function create($primitiveTags): self
+    {
+        $instance = new self();
+
+        if (is_array($primitiveTags)) {
+            foreach ($primitiveTags as $tag) {
+                $instance->tags[] = PostTag::create($tag);
+            }
+        }
+
+        return $instance;
+    }
+
+    /**
+     * 保持しているPostTagの配列を取得.
+     *
+     * @return mixed
+     */
+    public function toArray()
+    {
+        return $this->tags;
+    }
+}
