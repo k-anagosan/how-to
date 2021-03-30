33d4e727a960dc1c344d5d2303d5638b114a0455
k-anagosan <k.anagosan1027@gmail.com>
2021-03-30 22:46:16 +0900

    [fix]#95 Removed test on image submission
    * PostSubmitApiTest.phpから画像投稿に関するテストを削除しました。

--- laravel/tests/Feature/PostSubmitApiTest.php
+++ laravel/tests/Feature/PostSubmitApiTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
@@ -30,11 +29,6 @@ class PostSubmitApiTest extends TestCase
                 $this->faker->word,
                 $this->faker->word,
             ],
-            'photos' => [
-                UploadedFile::fake()->image("{$this->faker->word}.jpg"),
-                UploadedFile::fake()->image("{$this->faker->word}.jpg"),
-                UploadedFile::fake()->image("{$this->faker->word}.jpg"),
-            ],
         ];
     }
 
@@ -43,7 +37,6 @@ class PostSubmitApiTest extends TestCase
      */
     public function should_タイトルと文章だけの記事をアップロードできる(): void
     {
-        $this->data['photos'] = null;
         $this->data['tags'] = null;
 
         // アップロードを行う
@@ -66,49 +59,6 @@ class PostSubmitApiTest extends TestCase
         // S3に本文がファイルとして保存されているか
         Storage::cloud()->assertExists($post->content);
         $this->assertEquals(Storage::cloud()->get($post->content), $this->data['content']);
-    }
-
-    /**
-     * @test
-     */
-    public function should_画像付きの記事をアップロードできる(): void
-    {
-        $this->data['tags'] = null;
-
-        // アップロードを行う
-        $response = $this->actingAs($this->user)->postJson(route('post.create'), $this->data);
-
-        $response->assertStatus(201);
-
-        // レスポンスJSONが期待した構造であるか
-        $this->assertMatchesRegularExpression('/^[0-9a-zA-Z]{20}$/', $response->json('post_id'));
-
-        // アップロードによりPostsテーブルにレコードが保存されたか
-        $post = $this->user->posts->first();
-        $this->assertMatchesRegularExpression('/^[0-9a-zA-Z]{20}$/', $post->id);
-        $this->assertDatabaseHas($post->getTable(), [
-            'title' => $this->data['title'],
-            'content' => $post->content,
-            'user_id' => $this->user->id,
-        ]);
-
-        // S3に本文がファイルとして保存されているか
-        Storage::cloud()->assertExists($post->content);
-        $this->assertEquals(Storage::cloud()->get($post->content), $this->data['content']);
-
-        // Photoモデルのコレクションを取得
-        $photos = $post->photos;
-
-        foreach ($photos->all() as $photo) {
-            // 画像ファイル名がDBに保存されたか
-            $this->assertDatabaseHas($photo->getTable(), [
-                'post_id' => $photo->post_id,
-                'filename' => $photo->filename,
-            ]);
-
-            // 画像ファイルがS3に保存されたか
-            Storage::cloud()->assertExists($post->filename);
-        }
     }
 
     /**
@@ -116,8 +66,6 @@ class PostSubmitApiTest extends TestCase
      */
     public function should_タグ付きの記事をアップロードできる(): void
     {
-        $this->data['photos'] = null;
-
         // アップロードを行う
         $response = $this->actingAs($this->user)->postJson(route('post.create'), $this->data);
 
@@ -158,3 +106,11 @@ class PostSubmitApiTest extends TestCase
         }
     }
 }
+
+// 既に存在するタグ名の場合はTagNameテーブルに新規登録されない
+
+// 投稿できなかった場合は永続化したものを削除する
+
+// タイトルが無ければ投稿できない
+
+// 記事本文がなければ投稿できない
