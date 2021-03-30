02b44af6dbdc1c9da7d9d9b42dbc92219bb7d3c4
k-anagosan <k.anagosan1027@gmail.com>
2021-03-30 22:49:53 +0900

    [fix]#95 Removed workflow for image submission
    * PostItemUseCase.phpから画像投稿に関するワークフローを削除しました。

--- laravel/app/UseCase/PostItemUseCase.php
+++ laravel/app/UseCase/PostItemUseCase.php
@@ -2,13 +2,11 @@
 
 namespace App\UseCase;
 
-use App\Domain\Photo\Service\PhotoService;
 use App\Domain\Post\Service\PostItemService;
 use App\Domain\Tag\Service\TagService;
 use App\Domain\User\Service\AuthorService;
 use App\Domain\ValueObject\PostContent;
 use App\Domain\ValueObject\PostId;
-use App\Domain\ValueObject\PostPhotos;
 use App\Domain\ValueObject\PostTags;
 use App\Domain\ValueObject\PostTitle;
 
@@ -20,42 +18,34 @@ final class PostItemUseCase
 
     private $tagService;
 
-    private $photoService;
-
     public function __construct(
         AuthorService $authorService,
         PostItemService $postItemService,
-        TagService $tagService,
-        PhotoService $photoService
+        TagService $tagService
     ) {
         $this->authorService = $authorService;
         $this->postItemService = $postItemService;
         $this->tagService = $tagService;
-        $this->photoService = $photoService;
     }
 
     /**
      * @param PostTitle   $title
      * @param PostContent $content
      * @param PostTags    $tags
-     * @param PostPhotos  $photos
      *
      * @return PostId
      */
     public function execute(
         PostTitle $title,
         PostContent $content,
-        PostTags $tags,
-        PostPhotos $photos
+        PostTags $tags
     ) {
         $author = $this->authorService->getAuthor();
 
         $postedItem = $author->postItem($title, $content);
         $postedTags = $postedItem->postTags($tags);
-        $postedPhotos = $postedItem->postPhotos($photos);
 
         $this->tagService->saveTags($postedTags);
-        $this->photoService->savePhotos($postedPhotos);
         return $this->postItemService->saveItem($postedItem);
     }
 }
