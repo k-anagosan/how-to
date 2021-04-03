f4fcf3c6b43e50521a56a57e32f4b3d54c6807d4
k-anagosan <k.anagosan1027@gmail.com>
2021-04-04 04:00:52 +0900

    [add]#106 Created GetArticleDetailApiTest
    * 記事詳細取得APIを検証するテストコードを作成しました。
    * Postテーブルのファクトリを作成しました。

---
+++ laravel/database/factories/PostFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Post;
+use App\Models\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Post::class, function (Faker $faker) {
+    $id = Str::random(20);
+    return [
+        'id' => $id,
+        'user_id' => fn () => factory(User::class)->create()->id,
+        'title' => $faker->sentence,
+        'content' => $id . '.md',
+        'created_at' => $faker->dateTime(),
+        'updated_at' => $faker->dateTime(),
+    ];
+});
---
+++ laravel/tests/Feature/GetArticleDetailApiTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetArticleDetailApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function should_正しい構造のJSONが返される(): void
+    {
+        factory(Post::class)->create();
+        $post = Post::first();
+
+        $response = $this->getJson(route('post.show', ['id' => $post->id]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'id' => $post->id,
+                'url' => $post->url,
+                'author' => [
+                    'name' => $post->author->name,
+                ],
+            ]);
+    }
+}
