8190d42a4ccb65592bb841b2df233e20d9405da7
k-anagosan <k.anagosan1027@gmail.com>
2021-04-04 15:13:05 +0900

    [update]#106 Updated GetArticleDetailApiTest
    * S3にテスト用ファイルを保存する処理を追加しました。
    * 500エラーを検証するテストを作成しました。

--- laravel/tests/Feature/GetArticleDetailApiTest.php
+++ laravel/tests/Feature/GetArticleDetailApiTest.php
@@ -4,20 +4,36 @@ namespace Tests\Feature;
 
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class GetArticleDetailApiTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase,
+        WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Storage::fake('s3');
+    }
 
     /**
      * @test
      */
     public function should_正しい構造のJSONが返される(): void
     {
-        factory(Post::class)->create();
-        $post = Post::first();
+        $post = factory(Post::class)->create();
+
+        $file = UploadedFile::fake()->createWithContent($post->filename, $this->faker->text(2000));
+        Storage::cloud()->putFileAs('contents', $file, $post->filename, 'public');
+
+        // 前準備としてS3に$fileが保存されたか
+        Storage::cloud()->assertExists('contents/' . $post->filename);
 
         $response = $this->getJson(route('post.show', ['id' => $post->id]));
 
@@ -29,6 +45,8 @@ class GetArticleDetailApiTest extends TestCase
                     'name' => $post->author->name,
                 ],
             ]);
+
+        $this->assertEquals(Storage::cloud()->get('contents/' . $post->filename), $post->content);
     }
 
     /**
@@ -39,5 +57,16 @@ class GetArticleDetailApiTest extends TestCase
         $response = $this->getJson(route('post.show', ['id' => Str::random(20)]));
 
         $response->assertStatus(404);
+    }
+
+    /**
+     * @test
+     */
+    public function should_IDはあるがファイルが無い場合、500エラーが返される(): void
+    {
+        $post = factory(Post::class)->create();
+        $response = $this->getJson(route('post.show', ['id' => $post->id]));
+
+        $response->assertStatus(500);
     }
 }
