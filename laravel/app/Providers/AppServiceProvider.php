599c309453ecb88d255afc326b97ddf3fe750fac
k-anagosan <k.anagosan1027@gmail.com>
2021-04-04 15:10:02 +0900

    [add]#106 Created RepositoryServiceProvider
    * サービスコンテナにRepositoryInterfaceをバインドする処理をAppServiceProviderからRepositoryServiceProviderに移動しました。

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,20 +2,6 @@
 
 namespace App\Providers;
 
-use App\Domain\Photo\Repository\CloudPhotoRepositoryInterface;
-use App\Domain\Photo\Repository\EloquentPhotoRepository;
-use App\Domain\Photo\Repository\PhotoRepositoryInterface;
-use App\Domain\Photo\Repository\S3PhotoRepository;
-use App\Domain\Post\Repository\CloudContentRepositoryInterface;
-use App\Domain\Post\Repository\EloquentPostRepository;
-use App\Domain\Post\Repository\PostRepositoryInterface;
-use App\Domain\Post\Repository\S3ContentRepository;
-use App\Domain\Tag\Repository\EloquentTagNameRepository;
-use App\Domain\Tag\Repository\EloquentTagRepository;
-use App\Domain\Tag\Repository\TagNameRepositoryInterface;
-use App\Domain\Tag\Repository\TagRepositoryInterface;
-use App\Domain\User\Repository\EloquentUserRepository;
-use App\Domain\User\Repository\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,10 +11,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->authorRepositoryRegister();
-        $this->postRepositoryRegister();
-        $this->photoRepositoryRegister();
-        $this->tagRepositoryRegister();
     }
 
     /**
@@ -36,50 +18,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    }
-
-    private function authorRepositoryRegister(): void
-    {
-        $this->app->bind(
-            UserRepositoryInterface::class,
-            EloquentUserRepository::class
-        );
-    }
-
-    private function postRepositoryRegister(): void
-    {
-        $this->app->bind(
-            PostRepositoryInterface::class,
-            EloquentPostRepository::class
-        );
-        $this->app->bind(
-            CloudContentRepositoryInterface::class,
-            S3ContentRepository::class
-        );
-    }
-
-    private function photoRepositoryRegister(): void
-    {
-        $this->app->bind(
-            PhotoRepositoryInterface::class,
-            EloquentPhotoRepository::class
-        );
-        $this->app->bind(
-            CloudPhotoRepositoryInterface::class,
-            S3PhotoRepository::class
-        );
-    }
-
-    private function tagRepositoryRegister(): void
-    {
-        $this->app->bind(
-            TagNameRepositoryInterface::class,
-            EloquentTagNameRepository::class
-        );
-
-        $this->app->bind(
-            TagRepositoryInterface::class,
-            EloquentTagRepository::class
-        );
     }
 }
---
+++ laravel/app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use App\Domain\Photo\Repository\CloudPhotoRepositoryInterface;
+use App\Domain\Photo\Repository\EloquentPhotoRepository;
+use App\Domain\Photo\Repository\PhotoRepositoryInterface;
+use App\Domain\Photo\Repository\S3PhotoRepository;
+use App\Domain\Post\Repository\CloudContentRepositoryInterface;
+use App\Domain\Post\Repository\EloquentPostRepository;
+use App\Domain\Post\Repository\PostRepositoryInterface;
+use App\Domain\Post\Repository\S3ContentRepository;
+use App\Domain\Tag\Repository\EloquentTagNameRepository;
+use App\Domain\Tag\Repository\EloquentTagRepository;
+use App\Domain\Tag\Repository\TagNameRepositoryInterface;
+use App\Domain\Tag\Repository\TagRepositoryInterface;
+use App\Domain\User\Repository\EloquentUserRepository;
+use App\Domain\User\Repository\UserRepositoryInterface;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public $singletons = [
+        UserRepositoryInterface::class => EloquentUserRepository::class,
+        PostRepositoryInterface::class => EloquentPostRepository::class,
+        CloudContentRepositoryInterface::class => S3ContentRepository::class,
+        PhotoRepositoryInterface::class => EloquentPhotoRepository::class,
+        CloudPhotoRepositoryInterface::class => S3PhotoRepository::class,
+        TagNameRepositoryInterface::class => EloquentTagNameRepository::class,
+        TagRepositoryInterface::class => EloquentTagRepository::class,
+    ];
+
+    public function provides()
+    {
+        return [
+            UserRepositoryInterface::class,
+            PostRepositoryInterface::class,
+            CloudContentRepositoryInterface::class,
+            PhotoRepositoryInterface::class,
+            CloudPhotoRepositoryInterface::class,
+            TagNameRepositoryInterface::class,
+            TagRepositoryInterface::class,
+        ];
+    }
+}
