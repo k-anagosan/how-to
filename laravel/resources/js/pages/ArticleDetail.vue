3ea2d26430fe143106a835817e880b58c37e37b8
k-anagosan <k.anagosan1027@gmail.com>
2021-04-06 19:21:21 +0900

    [update]#105 Updated the design of article page
    * 記事ページレイアウトのプロトタイプを構築しました。
    * PCサイズの場合は2カラム表示となるように改修しました。

--- laravel/resources/js/pages/ArticleDetail.vue
+++ laravel/resources/js/pages/ArticleDetail.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="header-offset">
-    <h1 v-if="article">{{ article.title }}</h1>
-    <div v-if="article" class="md-preview-area" v-html="formattedContent"></div>
+  <div class="header-offset flex lg:px-16 sm:pb-8">
+    <article
+      v-if="article"
+      class="sm:shadow-md sm:p-10 p-4 pb-8 sm:rounded-lg bg-white lg:w-2/3 w-full"
+    >
+      <div class="title-area">
+        <h1 class="text-2xl">{{ article.title }}</h1>
+      </div>
+      <div class="md-preview-area" v-html="formattedContent"></div>
+    </article>
+    <aside v-if="article" class="lg:flex ml-6 hidden w-1/3 flex-col">
+      <ul class="tags shadow-md bg-white w-full p-4 mb-8 rounded-lg">
+        <li>tag1</li>
+        <li>tag1</li>
+        <li>tag1</li>
+      </ul>
+      <div class="author shadow-md bg-white w-full p-4 rounded-lg">
+        {{ article.author.name }}
+      </div>
+    </aside>
   </div>
 </template>
 
