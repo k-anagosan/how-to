47fe76051c473125b649d093cb5ff160d8336ade
k-anagosan <k.anagosan1027@gmail.com>
2021-04-08 13:26:09 +0900

    [update]#105 Created a prototype design during article load
    * 記事ロード中のページデザインを仮作成しました。

--- laravel/resources/js/pages/ArticleDetail.vue
+++ laravel/resources/js/pages/ArticleDetail.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="relative">
     <div
+      v-if="article"
       class="lg:hidden sticky z-50 top-0 bg-white border-b border-blue-100 shadow"
     >
       <div
@@ -8,7 +9,7 @@
       >
         <div class="py-4 flex justify-between items-center">
           <div class="rounded-full bg-black h-8 w-8 mr-2"></div>
-          <span v-if="article"> {{ article.author.name }}</span>
+          <span> {{ article.author.name }}</span>
         </div>
         <button
           type="button"
@@ -19,16 +20,19 @@
       </div>
     </div>
     <div class="container mx-auto flex flex-col lg:px-16 sm:p-8 pagetop-offset">
-      <div v-if="article" class="title-area p-8 text-center">
-        <h1 class="text-2xl">{{ article.title }}</h1>
+      <div class="title-area min-h-8 p-8 text-center">
+        <h1 v-if="article" class="text-2xl">{{ article.title }}</h1>
       </div>
 
       <div class="flex">
         <article
-          v-if="article"
-          class="sm:shadow-md sm:p-10 p-5 pb-8 sm:rounded-lg bg-white lg:w-2/3 w-full"
+          class="min-main-height sm:shadow-md sm:p-10 p-5 pb-8 sm:rounded-lg bg-white lg:w-2/3 w-full"
         >
-          <div class="md-preview-area" v-html="formattedContent"></div>
+          <div
+            v-if="article"
+            class="md-preview-area"
+            v-html="formattedContent"
+          ></div>
         </article>
         <aside v-if="article" class="lg:flex ml-6 hidden w-1/3 flex-col">
           <ul
