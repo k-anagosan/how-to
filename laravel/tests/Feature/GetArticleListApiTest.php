567b9fa7b650d686e5013bc5c388f565fcfdbb1e
k-anagosan <k.anagosan1027@gmail.com>
2021-04-10 17:41:52 +0900

    [add]#116 Added test code to validate pagination of article list
    * ページネーションがうまく機能しているか検証するテストコードを追加しました。

--- laravel/tests/Feature/GetArticleListApiTest.php
+++ laravel/tests/Feature/GetArticleListApiTest.php
@@ -14,7 +14,7 @@ class GetArticleListApiTest extends TestCase
     use RefreshDatabase,
         WithFaker;
 
-    private const POST_SIZE = 1000;
+    private const POST_SIZE = 101;
 
     public function setUp(): void
     {
@@ -34,7 +34,49 @@ class GetArticleListApiTest extends TestCase
 
         $response = $this->getJson(route('posts'));
 
-        $posts = Post::with(['author', 'tags.tagName'])->orderBy(Post::CREATED_AT, 'desc')->limit(10)->get();
+        $posts = Post::with(['author', 'tags.tagName'])
+            ->orderBy(Post::CREATED_AT, 'desc')
+            ->limit(10)
+            ->get();
+
+        $expect = $posts->map(function ($post) {
+            return [
+                'id' => $post->id,
+                'title' => $post->title,
+                'tags' => $post->tags->map(function ($tag) {
+                    return $tag->tagName;
+                })->toArray(),
+                'author' => [
+                    'name' => $post->author->name,
+                ],
+            ];
+        })->toArray();
+
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data')
+            ->assertJsonFragment(['data' => $expect]);
+    }
+
+    /**
+     * @test
+     */
+    public function should_ページのクエリストリングがある場合に正しい構造のJSONが返却される(): void
+    {
+        factory(Post::class, self::POST_SIZE)->create()->map(function ($post): void {
+            factory(TagName::class, 3)->create()->map(function ($tagName) use ($post): void {
+                factory(Tag::class)->create(['post_id' => $post->id, 'tag_name_id' => $tagName->id]);
+            });
+        });
+
+        $page = random_int(1, floor(self::POST_SIZE / 10));
+
+        $response = $this->getJson(route('posts', ['page' => $page]));
+
+        $posts = Post::with(['author', 'tags.tagName'])
+            ->orderBy(Post::CREATED_AT, 'desc')
+            ->limit(10)
+            ->offset(($page - 1) * 10)
+            ->get();
 
         $expect = $posts->map(function ($post) {
             return [
