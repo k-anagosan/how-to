d1f3ddec5b95a6945199e37cb724c3c4a6e29412
k-anagosan <k.anagosan1027@gmail.com>
2021-04-10 12:44:50 +0900

    [fix]#116 Fixed Tag Model
    * Tagモデルのidが文字列型として定義されていたので修正しました。
    * Tagもでるのidが自動増分OFFとして定義されていたので修正しました。

--- laravel/app/Models/Tag.php
+++ laravel/app/Models/Tag.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    public $incrementing = false;
-
-    protected $keyType = 'string';
-
     protected $visible = ['tagName'];
 
     public function post()
