16c97f271d862850ce1ece4af165e363356c172b
k-anagosan <k.anagosan1027@gmail.com>
2021-04-11 23:56:43 +0900

    [remove]#124 Temporarily removed the test code
    * 一時的にCardList.test.jsを削除しました。

--- laravel/resources/js/pages/CardList.test.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import CardList from "@/pages/CardList.vue";
-
-describe("CardList.vue", () => {
-    const wrapper = shallowMount(CardList);
-    it("正しいhtmlが出力される", () => {
-        expect(wrapper.html()).toContain("<h1>Card List</h1>");
-    });
-});
