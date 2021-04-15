8042761f96ed3136f9618438966ea7615506b8f5
k-anagosan <k.anagosan1027@gmail.com>
2021-04-16 06:22:53 +0900

    [add]#118 Created IconList Component
    * アイコンをリストで表示するコンポ―ネント、IconListを作成しました。
    * IconListを検証するテストコードを作成しました。

---
+++ laravel/resources/js/components/IconList.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul
+    class="grid gap-y-2"
+    :class="{ 'grid-cols-2 gap-x-2': direction === 'row' }"
+  >
+    <li v-for="icon in icons" :key="icon.name">
+      <Icon :icon="icon" :size="size" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import Icon from "../components/Icon.vue";
+export default {
+  components: {
+    Icon,
+  },
+  props: {
+    icons: {
+      type: Array,
+      required: true,
+    },
+    direction: {
+      type: String,
+      required: false,
+      default: "col",
+      validator(value) {
+        return ["row", "col"].indexOf(value) !== -1;
+      },
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "md",
+      validator(value) {
+        return ["sm", "md", "lg"].indexOf(value) !== -1;
+      },
+    },
+  },
+};
+</script>
