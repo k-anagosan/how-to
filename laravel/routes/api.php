d45eccb8632a5d0973db15907a60d3f5550af71f
k-anagosan <k.anagosan1027@gmail.com>
2021-04-18 17:18:12 +0900

    [add]#136 Added an endpoint for the "Like" API
    * いいね削除APIへのエンドポイントをapi.phpに追加しました。

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -20,6 +20,7 @@ Route::get('/user', fn () => Auth::check() ? Auth::user() : [])->name('user');
 Route::post('/post', App\Http\Actions\PostAction::class)->name('post.create');
 Route::get('/post/{id}', App\Http\Actions\GetArticleAction::class)->name('post.show');
 Route::put('/post/{id}/like', App\Http\Actions\LikeAction::class)->name('post.like');
+Route::delete('/post/{id}/unlike', App\Http\Actions\UnlikeAction::class)->name('post.unlike');
 
 Route::get('/posts', App\Http\Actions\GetArticleListAction::class)->name('posts');
 
