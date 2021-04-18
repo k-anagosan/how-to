42c6e43c168fb69cb49868989fc95b54de9bdbe5
k-anagosan <k.anagosan1027@gmail.com>
2021-04-18 10:21:17 +0900

    [update]#134 Introduced the Spinner Component
    * ArticleDetail、CardList、Edit、LoginコンポーネントにSpinnerコンポーネントを導入しました。
    * dataかcomputedにloadingを定義し、この値がtrueの時のみSpinnerを表示するように改修しました。
    * コードの見栄えを一部修正しました。

--- laravel/resources/js/pages/CardList.vue
+++ laravel/resources/js/pages/CardList.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="pagetop-offset">
-    <ul
-      v-if="list"
-      class="cardlist xl:mx-40 mx-4 grid lg:grid-rows-6 lg:grid-cols-3 sm:grid-rows-9 sm:grid-cols-2 sm:gap-6 gap-y-6"
-    >
-      <Card v-for="article in list" :key="article.id" :article="article" />
-    </ul>
-    <Pagination v-if="pagination" :pagination="pagination" />
+  <div>
+    <Spinner v-if="loading" class="pagetop-offset" />
+    <div v-if="!loading" class="pagetop-offset">
+      <ul
+        v-if="list"
+        class="cardlist xl:mx-40 mx-4 grid lg:grid-rows-6 lg:grid-cols-3 sm:grid-rows-9 sm:grid-cols-2 sm:gap-6 gap-y-6"
+      >
+        <Card v-for="article in list" :key="article.id" :article="article" />
+      </ul>
+      <Pagination v-if="pagination" :pagination="pagination" />
+    </div>
   </div>
 </template>
 <script>
 import Card from "../components/Card.vue";
 import Pagination from "../components/Pagination.vue";
+import Spinner from "../components/Spinner.vue";
 
 export default {
   components: {
     Card,
     Pagination,
+    Spinner,
   },
   props: {
     page: {
@@ -29,23 +34,23 @@ export default {
     return {
       list: null,
       pagination: null,
+      loading: false,
     };
   },
   watch: {
     $route: {
       async handler() {
         [this.list, this.pagination] = [null, null];
+        this.loading = true;
         await this.fetchArticleList();
+        this.loading = false;
       },
       immediate: true,
     },
   },
   methods: {
     async fetchArticleList() {
-      const response = await this.$store.dispatch(
-        "post/getArticleList",
-        this.page
-      );
+      const response = await this.$store.dispatch("post/getArticleList", this.page);
       if (!response) return;
       this.list = response.data;
       delete response.data;
--- laravel/resources/js/pages/Edit.vue
+++ laravel/resources/js/pages/Edit.vue
@@ -1,98 +1,72 @@
 <template>
   <div class="edit">
     <form id="edit" @submit.prevent="post">
-      <div class="flex flex-col main-height pagetop-offset">
-        <input
-          v-model="postForm.title"
-          type="text"
-          name="title"
-          class="block w-full border px-2 py-1"
-          placeholder="タイトル"
-        />
-        <input
-          v-model="tagsString"
-          type="text"
-          name="tags"
-          class="block w-full border px-2"
-          placeholder="タグ"
-        />
-        <ul
-          class="flex sm:justify-start justify-between items-center ml-2 h-8 mt-1"
-        >
-          <li class="flex justify-start h-full">
-            <ul class="flex justify-start h-full items-center">
-              <li class="h-full mr-2 relative">
-                <button
-                  id="write-tab"
-                  :class="{ 'text-gray-900 font-bold': tab === 1 }"
-                  class="flex justify-center items-center h-full px-4 text-gray-500 outline-none focus:outline-none"
-                  type="button"
-                  @click="tab = 1"
-                >
-                  Write
-                </button>
-                <span
-                  class="absolute bottom-0 h-0.5 w-full bg-gray-300"
-                  :class="{ 'bg-gray-900': tab === 1 }"
-                ></span>
-              </li>
-              <li class="h-full mr-2 relative">
-                <button
-                  id="preview-tab"
-                  :class="{ 'text-gray-900 font-bold': tab === 2 }"
-                  class="flex justify-center items-center h-full px-4 text-gray-500 outline-none focus:outline-none"
-                  type="button"
-                  @click="tab = 2"
-                >
-                  Preview
-                </button>
-                <span
-                  class="absolute bottom-0 h-0.5 w-full bg-gray-300"
-                  :class="{ 'bg-gray-900': tab === 2 }"
-                ></span>
-              </li>
-            </ul>
-          </li>
-          <li class="h-full flex items-center">
-            <input
-              id="post-photo"
-              type="file"
-              name="photo"
-              class="hidden"
-              value="Add Photo"
-              @change="onFileChange"
-            />
-            <label
-              for="post-photo"
-              class="h-full flex items-center px-4 rounded-sm focus:bg-white focus:text-gray-700 transition-colors cursor-pointer"
-              ><ion-icon name="image-outline"></ion-icon
-            ></label>
-          </li>
-        </ul>
-        <div
-          class="content flex-auto overflow-auto border shadow sm:rounded bg-white"
-        >
-          <div
-            v-show="tab === 1"
-            class="edit-content flex flex-col w-full h-full"
-          >
-            <textarea
-              v-model="postForm.content"
-              name="content"
-              class="block w-full resize-none p-4 flex-auto"
-              placeholder="共有したい知識をMarkdown記法で書いて投稿しましょう"
-            ></textarea>
-          </div>
-          <div v-show="tab === 2" class="preview-content w-full">
-            <MarkdownPreview
-              :text="postForm.content"
-              class="sm:p-10 p-4 break-words"
-            />
+      <Spinner v-if="loading" class="pagetop-offset" />
+      <div v-if="!loading">
+        <div class="flex flex-col main-height pagetop-offset">
+          <input
+            v-model="postForm.title"
+            type="text"
+            name="title"
+            class="block w-full border px-2 py-1"
+            placeholder="タイトル"
+          />
+          <input v-model="tagsString" type="text" name="tags" class="block w-full border px-2" placeholder="タグ" />
+          <ul class="flex sm:justify-start justify-between items-center ml-2 h-8 mt-1">
+            <li class="flex justify-start h-full">
+              <ul class="flex justify-start h-full items-center">
+                <li class="h-full mr-2 relative">
+                  <button
+                    id="write-tab"
+                    :class="{ 'text-gray-900 font-bold': tab === 1 }"
+                    class="flex justify-center items-center h-full px-4 text-gray-500 outline-none focus:outline-none"
+                    type="button"
+                    @click="tab = 1"
+                  >
+                    Write
+                  </button>
+                  <span class="absolute bottom-0 h-0.5 w-full bg-gray-300" :class="{ 'bg-gray-900': tab === 1 }"></span>
+                </li>
+                <li class="h-full mr-2 relative">
+                  <button
+                    id="preview-tab"
+                    :class="{ 'text-gray-900 font-bold': tab === 2 }"
+                    class="flex justify-center items-center h-full px-4 text-gray-500 outline-none focus:outline-none"
+                    type="button"
+                    @click="tab = 2"
+                  >
+                    Preview
+                  </button>
+                  <span class="absolute bottom-0 h-0.5 w-full bg-gray-300" :class="{ 'bg-gray-900': tab === 2 }"></span>
+                </li>
+              </ul>
+            </li>
+            <li class="h-full flex items-center">
+              <input id="post-photo" type="file" name="photo" class="hidden" value="Add Photo" @change="onFileChange" />
+              <label
+                for="post-photo"
+                class="h-full flex items-center px-4 rounded-sm focus:bg-white focus:text-gray-700 transition-colors cursor-pointer"
+                ><ion-icon name="image-outline"></ion-icon
+              ></label>
+            </li>
+          </ul>
+          <div class="content flex-auto overflow-auto border shadow sm:rounded bg-white">
+            <div v-show="tab === 1" class="edit-content flex flex-col w-full h-full">
+              <textarea
+                v-model="postForm.content"
+                name="content"
+                class="block w-full resize-none p-4 flex-auto"
+                placeholder="共有したい知識をMarkdown記法で書いて投稿しましょう"
+              ></textarea>
+            </div>
+            <div v-show="tab === 2" class="preview-content w-full">
+              <MarkdownPreview :text="postForm.content" class="sm:p-10 p-4 break-words" />
+            </div>
           </div>
         </div>
-      </div>
-      <div class="flex justify-end my-2 sm:mr-0 mr-2">
-        <Button id="post-btn" type="submit">Post</Button>
+        <div class="flex justify-end my-2 sm:mr-0 mr-2">
+          <Button id="post-btn" type="submit">Post</Button>
+        </div>
       </div>
     </form>
     <ErrorMessages :errors="errors" @clear="clearMessage" />
@@ -102,6 +76,7 @@
 import Button from "../components/SubmitButton.vue";
 import MarkdownPreview from "../components/MarkdownPreview.vue";
 import ErrorMessages from "../components/ErrorMessages.vue";
+import Spinner from "../components/Spinner.vue";
 import { mapState, mapGetters } from "vuex";
 
 export default {
@@ -109,6 +84,7 @@ export default {
     Button,
     MarkdownPreview,
     ErrorMessages,
+    Spinner,
   },
   data() {
     return {
@@ -119,6 +95,7 @@ export default {
       },
       tagsString: "",
       tab: 1,
+      loading: false,
     };
   },
   computed: {
@@ -141,7 +118,9 @@ export default {
   },
   methods: {
     async post() {
+      this.loading = true;
       const postId = await this.$store.dispatch("post/postItem", this.postForm);
+      this.loading = false;
       if (postId !== null) {
         this.clearMessage();
         this.$router.push(`article/${postId}`);
