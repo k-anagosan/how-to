1b1a868f20b2969327e2337cbda0b5cce5c9ecff
k-anagosan <k.anagosan1027@gmail.com>
2021-04-21 06:52:40 +0900

    [add]#44 Created Logout.vue
    * マウント直後に$router.replace("/")を実行するコンポーネント、Logout.vueを作成しました。
    * router.jsのroutersで、"/redirect"とLogout.vueを対応させました。

---
+++ laravel/resources/js/pages/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="pagetop-offset">logoutしています...</h1>
+</template>
+<script>
+export default {
+  mounted() {
+    this.replace();
+  },
+  methods: {
+    replace() {
+      this.$router.replace("/");
+    },
+  },
+};
+</script>
