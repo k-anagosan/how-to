fff75c7858579842a549223de3114f7a62142927
k-anagosan <k.anagosan1027@gmail.com>
2021-04-24 01:47:14 +0900

    [remove]#82 Removed unneeded files
    * Postのtagsへのリレーションを変更したことにより必要なくなったファイルとコードを削除しました。
    * いいね処理をPostRepositoryに移動したことにより必要なくなったファイルを削除しました。

--- laravel/app/Models/Tag.php
+++ laravel/app/Models/Tag.php
@@ -6,15 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    protected $visible = ['tagName'];
-
-    public function post()
-    {
-        return $this->belongsTo(Post::class);
-    }
-
-    public function tagName()
-    {
-        return $this->belongsTo(TagName::class);
-    }
 }
