108b5ccac5d5a4ff964719207a5fdce363bcf055
k-anagosan <k.anagosan1027@gmail.com>
2021-04-29 18:12:38 +0900

    [add]#159 Created UnfollowUser ADR classes
    * UnfollowUserのADRクラスを追加しました。

---
+++ laravel/app/Http/Responders/UnfollowUserResponder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Responders;
+
+use App\Domain\ValueObject\UserAccountId;
+use Illuminate\Http\JsonResponse;
+
+class UnfollowUserResponder
+{
+    /**
+     * @param null|UserAccountId $userId
+     * @return JsonResponse
+     */
+    public function response($userId): JsonResponse
+    {
+        if ($userId === null) {
+            abort(404);
+        }
+        return response()->json(['user_id' => $userId->toInt()], JsonResponse::HTTP_OK);
+    }
+}
