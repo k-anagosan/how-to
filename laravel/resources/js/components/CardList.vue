fefde499fd16fd5ab2dcdfab649054ba4c51fe30
k-anagosan <k.anagosan1027@gmail.com>
2021-05-13 10:43:26 +0900

    [update]#184 Added ownedByMe props in CardList.vue
    * CardList.vueのpropsに、表示する記事一覧が自身のものであるかを判別するownedByMeを追加しました。

--- laravel/resources/js/components/CardList.vue
+++ laravel/resources/js/components/CardList.vue
@@ -2,7 +2,13 @@
   <div>
     <slot></slot>
     <ul v-if="list" class="cardlist grid" :class="grid">
-      <Card v-for="article in list" :key="article.id" :article="article" @changeLike="onChangeLike" />
+      <Card
+        v-for="article in list"
+        :key="article.id"
+        :owned-by-me="ownedByMe"
+        :article="article"
+        @changeLike="onChangeLike"
+      />
     </ul>
   </div>
 </template>
@@ -22,6 +28,11 @@ export default {
       type: String,
       required: false,
       default: "lg:grid-cols-3 md:grid-cols-2 sm:gap-4 gap-y-6",
+    },
+    ownedByMe: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
   },
   methods: {
