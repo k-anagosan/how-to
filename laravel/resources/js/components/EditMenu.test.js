69fe44cc5f46a780200c28c8023bb7dac962199d
k-anagosan <k.anagosan1027@gmail.com>
2021-05-15 20:25:32 +0900

    [update]#219 Updated a test code for EditMenu.vue
    * Escキー押下により編集メニューが閉じられる処理のテストを追加しました。
    * deleteイベントハンドラ、deleteArticle()に関するテストを追加しました。

--- laravel/resources/js/components/EditMenu.test.js
+++ laravel/resources/js/components/EditMenu.test.js
@@ -4,8 +4,11 @@ import EditMenu from "@/components/EditMenu";
 import { randomStr } from "@/utils";
 
 const Test = new TestUtils();
+const spyOnClick = jest.spyOn(EditMenu.methods, "onClick");
+const spyOnKeyDown = jest.spyOn(EditMenu.methods, "onKeyDown");
 const spyCloseMenu = jest.spyOn(EditMenu.methods, "closeMenu");
-Test.setSpys({ spyCloseMenu });
+const spyDeleteArticle = jest.spyOn(EditMenu.methods, "deleteArticle");
+Test.setSpys({ spyOnClick, spyCloseMenu, spyOnKeyDown, spyDeleteArticle });
 
 let options = null;
 let wrapper = null;
@@ -15,6 +18,15 @@ beforeEach(() => {
         propsData: { articleId: randomStr() },
     };
     Test.setMountOption(EditMenu, options);
+    const post = {
+        namespaced: true,
+        actions: {
+            deleteItem: jest.fn().mockImplementation(() => randomStr(20)),
+        },
+    };
+    Test.setSpys({ deleteItem: post.actions.deleteItem });
+    Test.setVuex({ post });
+
     Test.setVueRouter();
 
     wrapper = Test.shallowWrapperFactory();
@@ -27,7 +39,7 @@ afterEach(() => {
     wrapper = null;
 });
 
-describe("表示関連", () => {
+describe("表示、操作関連", () => {
     it("ion-iconにid属性を正しく設定できる", () => {
         expect(wrapper.find("ion-icon-stub").attributes().id).toBe(`open-button-${wrapper.vm.articleId}`);
     });
@@ -54,21 +66,74 @@ describe("表示関連", () => {
         await wrapper.find(`#open-button-${wrapper.vm.articleId}`).vm.$emit("click");
         expect(wrapper.find(".edit-menu").exists()).toBe(false);
     });
+
+    it("削除ボタンをクリックしたらonClick()が実行される", async () => {
+        await wrapper.setData({ isShown: true });
+
+        expect(spyOnClick).not.toHaveBeenCalled();
+        await wrapper.find(".delete-button").trigger("click");
+        expect(spyOnClick).toHaveBeenCalled();
+    });
 });
 
 describe("メソッド関連", () => {
-    it("何かをクリックしたらcloseMenu()が実行される", () => {
-        const spyCloseMenu = jest.spyOn(EditMenu.methods, "closeMenu").mockImplementation(() => {});
-        expect(spyCloseMenu).not.toHaveBeenCalled();
-        window.dispatchEvent(new Event("click"));
-        expect(spyCloseMenu).toHaveBeenCalled();
-        spyCloseMenu.mockRestore();
+    describe("クリックイベント関連", () => {
+        it("何かをクリックしたらcloseMenu()が実行される", () => {
+            const spyCloseMenu = jest.spyOn(EditMenu.methods, "closeMenu").mockImplementation(() => {});
+            expect(spyCloseMenu).not.toHaveBeenCalled();
+            window.dispatchEvent(new Event("click"));
+            expect(spyCloseMenu).toHaveBeenCalled();
+            spyCloseMenu.mockRestore();
+        });
+
+        it("#open-button-xxx以外でcloseMenu()が実行されたらisShownにfalseが代入される", () => {
+            wrapper.setData({ isShown: true });
+            const e = { target: wrapper.vm.$el };
+            wrapper.vm.closeMenu(e);
+            expect(wrapper.vm.$data.isShown).toBe(false);
+        });
+
+        it("#open-button-xxxでcloseMenu()が実行されてもisShownにfalseは代入されない", () => {
+            wrapper.setData({ isShown: true });
+            const e = { target: wrapper.find(`#open-button-${wrapper.vm.articleId}`).vm.$el };
+            wrapper.vm.closeMenu(e);
+            expect(wrapper.vm.$data.isShown).toBe(true);
+        });
+
+        it("onClick()が実行されたらdeleteArticle()が実行される", async () => {
+            expect(spyDeleteArticle).not.toHaveBeenCalled();
+            await wrapper.vm.deleteArticle();
+            expect(spyDeleteArticle).toHaveBeenCalled();
+        });
+
+        it("deleteArticle()が実行されたらdeleteイベントが発火される", async () => {
+            expect(wrapper.emitted().delete).toBeUndefined();
+            await wrapper.vm.deleteArticle();
+            expect(wrapper.emitted().delete).toEqual([[]]);
+        });
     });
 
-    it("closeMenu()が実行されたらisShownにfalseが代入される", () => {
-        wrapper.setData({ isShown: true });
-        const e = { target: wrapper.vm.$el };
-        wrapper.vm.closeMenu(e);
-        expect(wrapper.vm.$data.isShown).toBe(false);
+    describe("キーダウンイベント関連", () => {
+        it("何かのキーを入力したらonKeyDown()が実行される", () => {
+            const spyOnKeyDown = jest.spyOn(EditMenu.methods, "onKeyDown").mockImplementation(() => {});
+            expect(spyOnKeyDown).not.toHaveBeenCalled();
+            window.dispatchEvent(new KeyboardEvent("keydown"));
+            expect(spyOnKeyDown).toHaveBeenCalled();
+            spyOnKeyDown.mockRestore();
+        });
+
+        it("'Escape'キー押下でonKeyDown()が実行されたらisShownにfalseが代入される", () => {
+            wrapper.setData({ isShown: true });
+            const e = { key: "Escape" };
+            wrapper.vm.onKeyDown(e);
+            expect(wrapper.vm.$data.isShown).toBe(false);
+        });
+
+        it("'Escape'キー以外の押下でonKeyDown()が実行されてもisShownにfalseは代入されない", () => {
+            wrapper.setData({ isShown: true });
+            const e = { key: "Enter" };
+            wrapper.vm.onKeyDown(e);
+            expect(wrapper.vm.$data.isShown).toBe(true);
+        });
     });
 });
