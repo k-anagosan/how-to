1b1ced8afdc28b8e3c0e0cb6d3cfe155d0ec81b1
k-anagosan <k.anagosan1027@gmail.com>
2021-05-27 21:24:50 +0900

    [fix]#159 Changed the test name of Unfollow API
    * フォロー解除APIのテスト名を`UnfollowApiTest`から`DeleteFollowApiTest`に変更しました。

--- laravel/tests/Feature/DeleteFollowUserApiTest.php
+++ laravel/tests/Feature/DeleteFollowUserApiTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class UnfollowUserApiTest extends TestCase
+class DeleteFollowUserApiTest extends TestCase
 {
     use RefreshDatabase,
         WithFaker;
@@ -40,7 +40,7 @@ class UnfollowUserApiTest extends TestCase
      */
     public function should_指定のユーザーをフォロー解除できる(): void
     {
-        $response = $this->actingAs($this->user)->deleteJson(route('user.unfollow', $this->followId));
+        $response = $this->actingAs($this->user)->deleteJson(route('user.delete.follow', $this->followId));
 
         $response->assertStatus(200)
             ->assertExactJson(['user_id' => $this->followId]);
@@ -58,7 +58,7 @@ class UnfollowUserApiTest extends TestCase
      */
     public function should_未ログインユーザーがフォロー解除を行ったら401エラーを返す(): void
     {
-        $response = $this->deleteJson(route('user.unfollow', $this->followId));
+        $response = $this->deleteJson(route('user.delete.follow', $this->followId));
 
         $response->assertStatus(401)->assertExactJson(['message' => 'Unauthenticated.']);
 
@@ -70,9 +70,9 @@ class UnfollowUserApiTest extends TestCase
      */
     public function should_複数回ユーザーのフォロー解除をしてもDBに1つしかレコードが挿入されない(): void
     {
-        $this->actingAs($this->user)->deleteJson(route('user.unfollow', $this->followId));
-        $this->actingAs($this->user)->deleteJson(route('user.unfollow', $this->followId));
-        $this->actingAs($this->user)->deleteJson(route('user.unfollow', $this->followId));
+        $this->actingAs($this->user)->deleteJson(route('user.delete.follow', $this->followId));
+        $this->actingAs($this->user)->deleteJson(route('user.delete.follow', $this->followId));
+        $this->actingAs($this->user)->deleteJson(route('user.delete.follow', $this->followId));
 
         $this->assertEquals(self::USER_NUMBER - 2, $this->user->follows()->count());
     }
@@ -82,7 +82,7 @@ class UnfollowUserApiTest extends TestCase
      */
     public function should_自身を指定してフォロー解除はできない(): void
     {
-        $response = $this->actingAs($this->user)->deleteJson(route('user.unfollow', $this->user->id));
+        $response = $this->actingAs($this->user)->deleteJson(route('user.delete.follow', $this->user->id));
 
         $expected = [
             'message' => 'The given data was invalid.',
@@ -102,7 +102,7 @@ class UnfollowUserApiTest extends TestCase
      */
     public function should_指定のユーザーが存在しなかったら404エラーを返す(): void
     {
-        $response = $this->actingAs($this->user)->deleteJson(route('user.unfollow', self::USER_NUMBER + 1));
+        $response = $this->actingAs($this->user)->deleteJson(route('user.delete.follow', self::USER_NUMBER + 1));
 
         $response->assertStatus(404);
     }
