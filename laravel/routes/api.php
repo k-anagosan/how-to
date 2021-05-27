0a8c15a8baed13831c79cec2b0c64c1b5b926c9a
k-anagosan <k.anagosan1027@gmail.com>
2021-05-27 21:14:30 +0900

    [fix]#159 Fix the name of the unfollowing API
    * いいね解除APIの名前を`unfollow`から`delete.follow`に変更しました。
    * URLやルート名に重複があるルーティングをグループ化しました。

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -16,20 +16,26 @@ Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login')->name('
 Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
 Route::get('/user', fn () => Auth::check() ? Auth::user() : [])->name('user');
-Route::get('/user/{name}', App\Http\Actions\GetUserPageDataAction::class)->name('user.data');
-Route::get('/user/{name}/articles', App\Http\Actions\GetUserArticleListAction::class)->name('user.show');
-Route::get('/user/{name}/likes', App\Http\Actions\GetLikedArticleListAction::class)->name('user.likes');
-Route::get('/user/{name}/followers', App\Http\Actions\GetFollowerListAction::class)->name('user.followers');
 
-Route::put('/user/{id}/follow', App\Http\Actions\FollowUserAction::class)->name('user.follow');
-Route::delete('/user/{id}/follow', App\Http\Actions\UnfollowUserAction::class)->name('user.unfollow');
+Route::name('user.')->prefix('user')->group(function (): void {
+    Route::get('{name}', App\Http\Actions\GetUserPageDataAction::class)->name('data');
+    Route::get('{name}/articles', App\Http\Actions\GetUserArticleListAction::class)->name('show');
+    Route::get('{name}/likes', App\Http\Actions\GetLikedArticleListAction::class)->name('likes');
+    Route::get('{name}/followers', App\Http\Actions\GetFollowerListAction::class)->name('followers');
+
+    Route::put('{id}/follow', App\Http\Actions\FollowUserAction::class)->name('follow');
+    Route::delete('{id}/follow', App\Http\Actions\DeleteFollowUserAction::class)->name('delete.follow');
+});
 
 Route::post('/post', App\Http\Actions\PostAction::class)->name('post.create');
-Route::get('/post/{id}', App\Http\Actions\GetArticleAction::class)->name('post.show');
-Route::delete('/post/{id}', App\Http\Actions\DeleteArticleAction::class)->name('post.delete');
 
-Route::put('/post/{id}/like', App\Http\Actions\LikeAction::class)->name('post.like');
-Route::delete('/post/{id}/like', App\Http\Actions\DeleteLikeAction::class)->name('post.delete.like');
+Route::name('post.')->prefix('post')->group(function (): void {
+    Route::get('{id}', App\Http\Actions\GetArticleAction::class)->name('show');
+    Route::delete('{id}', App\Http\Actions\DeleteArticleAction::class)->name('delete');
+
+    Route::put('{id}/like', App\Http\Actions\LikeAction::class)->name('like');
+    Route::delete('{id}/like', App\Http\Actions\DeleteLikeAction::class)->name('delete.like');
+});
 
 Route::get('/posts', App\Http\Actions\GetArticleListAction::class)->name('posts');
 
