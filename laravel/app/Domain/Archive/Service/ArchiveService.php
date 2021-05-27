df44d60645a561b15d661361dd4aa6c93bba2375
k-anagosan <k.anagosan1027@gmail.com>
2021-05-28 00:44:26 +0900

    [add]#157 Created ArchiveService
    * アーカイブに関する処理を担うクラス、ArchiveServiceを作成しました。

---
+++ laravel/app/Domain/Archive/Service/ArchiveService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Domain\Archive\Service;
+
+use App\Domain\Archive\Entity\ArchiveEntity;
+use App\Domain\Post\Repository\PostRepositoryInterface as PostRepository;
+use App\Domain\ValueObject\PostId;
+
+class ArchiveService
+{
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    /**
+     * ArchiveEntityをもとに記事をアーカイブする。
+     *
+     * @param ArchiveEntity $archiveEntity
+     * @return null|PostId
+     */
+    public function putArchive(ArchiveEntity $archiveEntity)
+    {
+        return $this->postRepository->putArchive($archiveEntity->getPostId(), $archiveEntity->getUserId());
+    }
+}
