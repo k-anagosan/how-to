aaf7807667f6b67523790f1edded6f7f801f99af
k-anagosan <k.anagosan1027@gmail.com>
2021-05-29 03:57:03 +0900

    [add]#245 Created ArchiveButton Component

---
+++ laravel/resources/js/components/ArchiveButton.vue
@@ -0,0 +1,77 @@
+<template>
+  <button
+    type="button"
+    class="rounded-full bg-blue-200 flex justify-center items-center outline-none focus:outline-none hover:shadow-lg text-red transition-shadow"
+    :class="`icon-size-${size}`"
+    @click.stop="onClick"
+  >
+    <ion-icon
+      v-if="!isArchived"
+      :class="{ 'text-xl': size === 'lg' }"
+      class="transition-colors"
+      name="bookmark-outline"
+    ></ion-icon>
+    <ion-icon
+      v-if="isArchived"
+      :class="{ 'text-xl': size === 'lg' }"
+      class="transition-colors"
+      name="bookmark"
+    ></ion-icon>
+  </button>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default: "md",
+      validator(value) {
+        return ["sm", "md", "lg"].indexOf(value) !== -1;
+      },
+    },
+    isArchived: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      disabled: false,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      username: "auth/username",
+    }),
+  },
+  methods: {
+    onClick() {
+      if (!this.username) {
+        this.$router.push("/login");
+        return;
+      }
+      if (this.disabled) return;
+      this.disabled = true;
+      this.$emit("archive", { isArchived: !this.isArchived });
+
+      setTimeout(() => {
+        this.enable();
+      }, 1000);
+    },
+    enable() {
+      this.disabled = false;
+    },
+  },
+};
+</script>
+
+<style>
+button:hover ion-icon[name="bookmark-outline"],
+ion-icon[name="bookmark"] {
+  color: rgba(16, 185, 129, 1);
+}
+</style>
